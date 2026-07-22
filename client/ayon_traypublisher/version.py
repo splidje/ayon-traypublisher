9c086b1841d0d2957351fe0f9b73d28228b8cdfd
splidje <jonathan.hearn@ajptechnical.com>
2026-07-22 12:28:23 +0100

    tagging version 0.4.4+ubp0.1.0

--- client/ayon_traypublisher/version.py
+++ client/ayon_traypublisher/version.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 """Package declaring AYON addon 'traypublisher' version."""
-__version__ = "0.4.4+dev"
+__version__ = "0.4.4+ubp0.1.0"
--- package.py
+++ package.py
@@ -1,6 +1,6 @@
 name = "traypublisher"
 title = "TrayPublisher"
-version = "0.4.4+dev"
+version = "0.4.4+ubp0.1.0"
 app_host_name = "traypublisher"
 client_dir = "ayon_traypublisher"
 
